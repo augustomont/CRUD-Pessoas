beac784238eb614df46dfd18e6c6631a63036738
Augusto Monteiro <augustoengenharia@outlook.com>
2023-05-17 20:55:23 -0300

    Model  Pessoas Criada

---
+++ Pessoas/Pessoas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pessoas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
